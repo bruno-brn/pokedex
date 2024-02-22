90f704642be5b0c466ce0c3af240cbed227ed7b6
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2024-02-22 16:53:26 -0300

    conexão com a api pokedex, e ajuste de pages

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
   <div id="app">
-
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/poke.png" alt="">
+      <hr>
+      <h4 class="is-size-4">Pokedex</h4>
+      <input type="text">
+      <div v-for="(poke, index) in pokemons" :key="index">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index +1"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios'
+import Pokemon from './components/PokemonList.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      pokemons:[]
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=152&offset=0").then(res => {
+      console.log(res.data.results)
+      this.pokemons = res.data.results
+    })
+  },
+  components: {
+    Pokemon
+  }
 }
 </script>
 
